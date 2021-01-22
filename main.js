26356e29f23409ab5f1adc3dfe0039df70aa44a8
Willmer Obed Fúneme San Martín <willmer.funeme@unac.edu.co>
2021-01-21 20:53:58 -0500

    Ejercicio Clase

---
+++ main.js
@@ -0,0 +1,18 @@
+function saludar(p1){
+    console.log(p1);
+}
+
+function terminar(){
+    console.log("Programa finalizado")
+}
+
+
+function despedir(){
+    console.log("Adios usuario :)");
+}
+
+let dato = prompt("Ingrese el nombre");
+saludar(dato);
+terminar()
+despedir();
+
